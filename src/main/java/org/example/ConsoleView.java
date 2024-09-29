5e03fd8a23d85b44d774afcdc28d64b7138eb38a
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-09-29 18:18:59 -0500

    Connect 4 game

--- src/main/java/org/example/ConsoleView.java
+++ src/main/java/org/example/ConsoleView.java
@@ -2,8 +2,17 @@ package org.example;
 
 import java.util.Scanner;
 
+/**
+ * Console-based implementation of the Connect4View interface.
+ */
 public class ConsoleView implements Connect4View {
     private Scanner scanner = new Scanner(System.in);
+
+    /**
+     * Displays the game board in the console.
+     *
+     * @param board a 2D array representing the current game state.
+     */
     @Override
     public void printBoard(int[][] board) {
         for (int i = 0; i < board.length; i++) {
@@ -24,11 +33,21 @@ public class ConsoleView implements Connect4View {
         System.out.println();
     }
 
+    /**
+     * Prints a message to the user in the console.
+     *
+     * @param message the message to be displayed.
+     */
     @Override
     public void printMessage(String message) {
         System.out.println(message);
     }
 
+    /**
+     * Reads the user's input from the console to select a column.
+     *
+     * @return the column number chosen by the user.
+     */
     @Override
     public int getUserInput() {
         return scanner.nextInt();
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,15 @@
 package org.example;
 
+/**
+ * Main class for running the Connect 4 game.
+ */
 public class Main {
+
+    /**
+     * Main method to start the Connect 4 game.
+     *
+     * @param args command line arguments (not used).
+     */
     public static void main(String[] args) {
         Connect4Model model = new Connect4Model();
         Connect4View view = new ConsoleView();
