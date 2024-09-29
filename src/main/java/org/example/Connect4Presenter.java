97c809f2eeaceca2a1c4ef40f2d82ca605fdf6b8
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-09-29 18:18:26 -0500

    Connect 4 - Presenter class for the Connect 4 game, handling communication between the model and the view

--- src/main/java/org/example/Connect4Presenter.java
+++ src/main/java/org/example/Connect4Presenter.java
@@ -1,13 +1,26 @@
 package org.example;
 
+/**
+ * Presenter class for the Connect 4 game, handling communication between the model and the view.
+ */
 public class Connect4Presenter {
     private Connect4Model model;
     private Connect4View view;
 
+    /**
+     * Initializes the presenter with a model and a view.
+     *
+     * @param model the Connect 4 model.
+     * @param view the view that interacts with the user.
+     */
     public Connect4Presenter(Connect4Model model, Connect4View view) {
         this.model = model;
         this.view = view;
     }
+
+    /**
+     * Starts the Connect 4 game loop, handling user inputs and checking for a winner.
+     */
     public void startGame() {
         boolean gameWon = false;
 
